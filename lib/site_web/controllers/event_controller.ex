74177135d33a8a5d1163d16f4ef9f5fbb397a556
Will Ockelmann-Wagner <wow@carbonfive.com>
2018-12-30 12:07:42 -0700

    fix links

--- lib/site_web/controllers/event_controller.ex
+++ lib/site_web/controllers/event_controller.ex
@@ -9,6 +9,10 @@ defmodule SiteWeb.EventController do
     render(conn, "2019_la_conference.html", year: 2019, title: "EMPEX LA 2019")
   end
 
+  def nyc(conn, _params) do
+    render(conn, "2018_nyc_conference.html", year: 2018, title: "EMPEX NYC 2018")
+  end
+
   def show(conn, %{"id" => slug}) do
     [city, type, year] = String.split(slug, "-")
     render(conn, "#{year}_#{city}_#{type}.html", year: year)
